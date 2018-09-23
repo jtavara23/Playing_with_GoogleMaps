bfe20882b430abb855f96db476b90d43c37968f6
jtavara23 <joshuatavara23@gmail.com>
2018-09-23 17:21:16 -0500

    create InfoWindow to display marker's content

--- mapas.js
+++ mapas.js
@@ -5,26 +5,26 @@ var markers = [
         label: 'T',
         position: {lat:-8.1120408, lng:-79.028485},
         icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content: '<h1>Capital of La Libertad</h1>'
+        content: '<h4>Capital of La Libertad</h4>'
     },
     {
         name: 'La Esperanza',
         position: {lat:-8.058065, lng:-79.055249},
-        content: '<h1>La Esperanza Disctrict</h1>'
+        content: '<h4>La Esperanza Disctrict</h4>'
     },
     {
         name: 'Huanchaco District',
         position: {lat:-8.086244, lng:-79.092896},
         label: 'H',
         icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content: '<h1>Huanchaco</h1>'
+        content: '<h4>Huanchaco</h4>'
         
     },
     {
         name: 'Lima, Peru',
         position: {lat: -12.049, lng: -77.0282},
         icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content: '<h1>Capital of Peru</h1>'
+        content: '<h4>Capital of Peru</h4>'
     }
 ]
 
@@ -32,6 +32,7 @@ function createMarker(marker, mapa, draggable_st=false){
     return new google.maps.Marker({
         position: marker.position,
         map: mapa,
+        content: marker.content,
         icon: marker.icon,
         shape: marker.shape,
         title: marker.name,
@@ -40,6 +41,12 @@ function createMarker(marker, mapa, draggable_st=false){
     });
 }
 
+function createInfoWindow(marker){
+    return new google.maps.InfoWindow({
+        content :marker.content,
+        maxWidth: 500
+    });
+}
 
 function initMap(){
     var charac_Map1 ={
@@ -56,12 +63,24 @@ function initMap(){
     var mapa1 = new google.maps.Map( document.getElementById('map1'), charac_Map1);
     var mapa2 = new google.maps.Map( document.getElementById('map2'), charac_Map2 );
 
-    createMarker(markers[0],mapa1);
-    createMarker(markers[1],mapa1);
-    createMarker(markers[2],mapa1,true);
-    createMarker(markers[3],mapa2);
+
+    created_markers = [];
+    created_markers.push(createMarker(markers[0],mapa1));
+    created_markers.push(createMarker(markers[1],mapa1));
+    created_markers.push(createMarker(markers[2],mapa1,true));
+    created_markers.push(createMarker(markers[3],mapa2));
+
+    created_markers.forEach(function(marker){
+        marker.addListener('click', function(){
+            //console.log(marker.content);
+            createInfoWindow(marker).open(marker.map, marker);
+        });
+    });
+
 
     
+
+
     var geocoder = new google.maps.Geocoder();
 
     document.getElementById('submit').addEventListener('click', function() {
