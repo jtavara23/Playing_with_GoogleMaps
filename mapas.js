2df8a3af66d0f284200216171f47900b4e1d53e1
jtavara23 <joshuatavara23@gmail.com>
2018-09-23 16:56:10 -0500

    create dynamic markers

--- mapas.js
+++ mapas.js
@@ -1,46 +1,66 @@
 
 var markers = [
     {
+        name: 'Trujillo', 
+        label: 'T',
         position: {lat:-8.1120408, lng:-79.028485},
-        iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachfalag.png',
-        content: '<h1>Trux</h1>'
+        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content: '<h1>Capital of La Libertad</h1>'
     },
     {
+        name: 'La Esperanza',
         position: {lat:-8.058065, lng:-79.055249},
-        content: '<h1>La esperanza</h1>'
+        content: '<h1>La Esperanza Disctrict</h1>'
     },
     {
-        position: {lat:-8.1160, lng:-79.03},
-        iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachfalag.png',
+        name: 'Huanchaco District',
+        position: {lat:-8.086244, lng:-79.092896},
+        label: 'H',
+        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
         content: '<h1>Huanchaco</h1>'
+        
+    },
+    {
+        name: 'Lima, Peru',
+        position: {lat: -12.049, lng: -77.0282},
+        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content: '<h1>Capital of Peru</h1>'
     }
 ]
 
-
-
+function createMarker(marker, mapa, draggable_st=false){
+    return new google.maps.Marker({
+        position: marker.position,
+        map: mapa,
+        icon: marker.icon,
+        shape: marker.shape,
+        title: marker.name,
+        label: marker.label,
+        draggable: draggable_st
+    });
+}
 
 
 function initMap(){
-    var optionsMap1 ={
-        zoom:14,
+    var charac_Map1 ={
+        zoom:12,
         center: {lat:-8.11, lng: -79.03}
     };
-    
-    var mapa1 = new google.maps.Map( document.getElementById('map1'), optionsMap1);
-    
-    var marker = new google.maps.Marker(
-        {position: {lat:-8.1120408, lng:-79.028485},
-        map : mapa1
-        }
-    );
 
-    var optionsMap2 = {
+    var charac_Map2 = {
         zoom: 10,
         center: {lat: -12.049, lng: -77.0282}
         //mapTypeId: google.maps.MapTypeId.SATELLITE,
     };
 
-    var mapa2 = new google.maps.Map( document.getElementById('map2'), optionsMap2 );
+    var mapa1 = new google.maps.Map( document.getElementById('map1'), charac_Map1);
+    var mapa2 = new google.maps.Map( document.getElementById('map2'), charac_Map2 );
+
+    createMarker(markers[0],mapa1);
+    createMarker(markers[1],mapa1);
+    createMarker(markers[2],mapa1,true);
+    createMarker(markers[3],mapa2);
+
     
     var geocoder = new google.maps.Geocoder();
 
